41c03f0e73fc1226205a44f9c4b27b2208899315
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-01-02 23:56:03 +0630

    feat: Button variant

--- ui-library/src/stories/Button.stories.tsx
+++ ui-library/src/stories/Button.stories.tsx
@@ -14,7 +14,21 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Solid: Story = {
+  args: {
+    children: "CLICK ME",
+    variant: "solid",
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    children: "CLICK ME",
+    variant: "outline",
+  },
+};
+
+export const Ghost: Story = {
   args: {
     children: "CLICK ME",
   },
