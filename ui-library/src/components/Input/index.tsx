b5c2f41cae24a03d9cbee937e5d13296499fb4d0
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-01-03 12:04:30 +0630

    feat: Input stories

---
+++ ui-library/src/components/Input/index.tsx
@@ -0,0 +1,34 @@
+import { VariantProps, cva } from "class-variance-authority";
+import { ComponentProps, forwardRef } from "react";
+import { cn } from "@/utils";
+
+const cvaClasses = cva([
+  "w-full",
+  "border",
+  "border-gray-200",
+  "p-2",
+  "rounded-lg",
+  "transition-all",
+  "duration-100",
+  "outline-none",
+  "focus:border-primary-500",
+  "placeholder:text-gray-400",
+  "placeholder:text-sm",
+]);
+
+type InputProps = VariantProps<typeof cvaClasses> & ComponentProps<"input">;
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        type="text"
+        className={cn(cvaClasses({ className }))}
+        {...props}
+      />
+    );
+  }
+);
+
+export default Input;
---
+++ ui-library/src/stories/Input.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Input from "@/components/Input";
+
+const meta: Meta<typeof Input> = {
+  title: "Input",
+  component: Input,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Text: Story = {
+  args: {
+    type: "text",
+    placeholder: "John Doe",
+    className: "border border-red-800",
+  },
+};
+
+export const Password: Story = {
+  args: {
+    type: "password",
+    placeholder: "********",
+  },
+};
+
+export const Number: Story = {
+  args: {
+    type: "number",
+    placeholder: "10000",
+  },
+};
+
+export const Radio: Story = {
+  args: {
+    type: "radio",
+  },
+};
+
+export const Date: Story = {
+  args: {
+    type: "date",
+  },
+};
